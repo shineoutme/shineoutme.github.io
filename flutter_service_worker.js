d739f928d635881070c43b57c42bb4099de1353a
Saran Nonkamjan <saran.non@gmail.com>
2021-11-25 12:54:25 +0700

    --

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "047a1c7b24918aa76e9142bf6f788879",
-"index.html": "7d351f9ae3b05b5a8886558f574c5cbd",
-"/": "7d351f9ae3b05b5a8886558f574c5cbd",
+"index.html": "2b0b3a4f82927f4063d396f0cf36d944",
+"/": "2b0b3a4f82927f4063d396f0cf36d944",
 "main.dart.js": "ce18aa36a3467693a9db2bad69d92ac5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -13,18 +13,18 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "da5d5ffc1acf42d638d6789896bb7d51",
-"assets/AssetManifest.json": "74a7772da99de69ad0d8e9c66985f015",
+"assets/AssetManifest.json": "83f75b00d474fd100df332bc0ee98a65",
 "assets/NOTICES": "fb37b57a46f8c840be0c7a7ae2db302f",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/images/lineman-logo.jpeg": "b44ec2f3732371fa008ab5e943e029ed",
 "assets/assets/images/flutter_logo.png": "478970b138ad955405d4efda115125bf",
+"assets/assets/images/2.0x/lineman-logo.jpeg": "0842889290e95f9307d85db01db45f67",
 "assets/assets/images/2.0x/flutter_logo.png": "4efb9624185aff46ca4bf5ab96496736",
-"assets/assets/images/2.0x/lineman-logo@2x.jpeg": "0842889290e95f9307d85db01db45f67",
-"assets/assets/images/2.0x/robinhood-logo@2x.jpeg": "72832417aa97820a504533c1b08b4681",
+"assets/assets/images/2.0x/robinhood-logo.jpeg": "72832417aa97820a504533c1b08b4681",
+"assets/assets/images/3.0x/lineman-logo.jpeg": "5f1755667fe125612c30e995226d0c4c",
 "assets/assets/images/3.0x/flutter_logo.png": "b8ead818b15b6518ac627b53376b42f2",
-"assets/assets/images/3.0x/lineman-logo@3x.jpeg": "5f1755667fe125612c30e995226d0c4c",
-"assets/assets/images/3.0x/robinhood-logo@3x.jpeg": "68298ea0c383bf7c0170ead3ef2c1267",
+"assets/assets/images/3.0x/robinhood-logo.jpeg": "68298ea0c383bf7c0170ead3ef2c1267",
 "assets/assets/images/robinhood-logo.jpeg": "238fffdf3651dacdb5b88649a9fe2a1e"
 };
 
